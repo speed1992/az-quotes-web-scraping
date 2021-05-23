8d40154025b8c49a8ce5109ec735ac7f1f36304c
Paras Anand <qtparas@gmail.com>
2021-05-23 21:38:29 +0530

    refactored code, more scalable now

---
+++ constants/constants.js
@@ -0,0 +1,14 @@
+module.exports.PHILOSOPHERS_DATA = [
+
+    {
+        URL: "https://www.azquotes.com/author/13003-Jean_Paul_Sartre",
+        philosopherNameInSelector: "Jean-Paul Sartre",
+        lastPage: 19,
+    },
+    {
+        URL: "https://www.azquotes.com/author/11564-Fernando_Pessoa",
+        philosopherNameInSelector: "Fernando Pessoa",
+        lastPage: 13,
+    }
+
+]
