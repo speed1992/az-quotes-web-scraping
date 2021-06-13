3b46580f2a4fb544afcf2fb089c1955becb06a62
Paras Anand <qtparas@gmail.com>
2021-06-13 12:50:52 +0530

    resolved the quotes collection bug

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const { requestURL, writeToFile, findOutLastPage } = require('./utils/utils');
 (async function () {
 
   console.log('App started');
-  let quotesCollection = []
 
   for (j = 0; j < PHILOSOPHERS_DATA.length; j++) {
+
+    let quotesCollection = []
 
     let { lastPage, philosopherNameInSelector } = await findOutLastPage(PHILOSOPHERS_DATA[j].url);
 
     for (i = 1; i <= lastPage; i++) {
 
-      console.log("Page No. ", i);
+      console.log("Page No.", i);
 
       const urlWithPageNumber = PHILOSOPHERS_DATA[j].url + "?p=" + i
 
