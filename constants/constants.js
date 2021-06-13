48f57756e9356bc540a71966bf75c973980d691f
Paras Anand <qtparas@gmail.com>
2021-06-13 12:09:55 +0530

    removed unecessary vars in package.json

--- constants/constants.js
+++ constants/constants.js
@@ -1,5 +1,9 @@
 module.exports.PHILOSOPHERS_DATA = [
-    { url: "https://www.azquotes.com/author/2957-Carl_von_Clausewitz", varName: "CLAUSEWITZ" }
+    {
+        url: "https://www.azquotes.com/author/9882-Marshall_McLuhan", varName: "MARSHALL_MCLUHAN",
+        url: "https://www.azquotes.com/author/11361-Blaise_Pascal", varName: "PASCAL",
+        url: "https://www.azquotes.com/author/12497-Francois_de_La_Rochefoucauld", varName: "ROCHEFOUCAULD"
+    }
 ]
 
 module.exports.STATIC_SUBSTRING1 = "export const "
