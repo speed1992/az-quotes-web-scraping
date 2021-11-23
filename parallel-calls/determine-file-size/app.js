ac17b14114c74243f8ea09004069c8af98b07185
Paras Anand <speed1992@gmail.com>
2021-11-24 01:04:18 +0530

    refactor

--- parallel-calls/determine-file-size/app.js
+++ parallel-calls/determine-file-size/app.js
@@ -14,9 +14,9 @@ function determineFileSize() {
                 method: 'GET'
                 , uri: 'https://www.azquotes.com/author/2398-Albert_Camus'
             }
-            , function (error, response, body) {
-                console.log(body.length)
-                resolve(body.length);
+            , function (error, response, html) {
+                console.log(html.length)
+                resolve(html.length);
             })
     });
 
