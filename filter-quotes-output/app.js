f639d04618ea93fe24ab05becc4f0f416567488d
Paras Anand <speed1992@gmail.com>
2021-12-10 16:47:07 +0530

    WIP: working on filter module

--- filter-quotes-output/app.js
+++ filter-quotes-output/app.js
@@ -1,5 +1,26 @@
 // Remove all urdu quotes
 // Remove all single words
 // Remove spanish quotes
+var path = require('path');
+var walk = require('walk');
+var fs = require('fs');
+const inputDirPath = path.resolve("../combine-output/output")
 
+const walker = walk.walk(inputDirPath);
 
+walker.on('file', function (root, fileStats, next) {
+    fs.readFile(inputDirPath + "/" + fileStats.name, 'utf8', function (err, data) {
+        if (err) throw err;
+        obj = JSON.parse(data);
+        console.log(obj[0])
+        next()
+    });
+});
+
+walker.on('errors', function (root, nodeStatsArray, next) {
+    next();
+});
+
+walker.on('end', function () {
+    console.log('all done');
+});
