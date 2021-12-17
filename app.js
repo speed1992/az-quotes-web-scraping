aba08216305aedd0c04b6940f514ee5339d9ab68
Paras Anand <speed1992@gmail.com>
2021-12-17 06:10:00 +0530

    delete module working fine

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const { start: pullQuotesFromAZQuotes } = require("./azquotes/app");
 const { start: combineOutputs } = require("./combine-output/app");
 const { start: removeDuplicateQuotes } = require("./string-similarity/app");
 const { start: pullQuotesFromGoodreads } = require("./goodreads/app");
+const { deleteOutputDirectories } = require("./delete-clear-outputs/app");
 
 (async function () {
+    await deleteOutputDirectories(["azquotes", "goodreads", "combine-output"])
     await pullQuotesFromAZQuotes();
     await pullQuotesFromGoodreads();
     console.log("Combining process started.")
--- delete-clear-outputs/app.js
+++ delete-clear-outputs/app.js
@@ -1,2 +1,23 @@
-// fs.rmdirSync(dir, { recursive: true });
+const fse = require('fs-extra');
+const path = require('path')
+
+module.exports.deleteOutputDirectories = function (moduleNames) {
+    return new Promise((resolve, reject) => {
+        const promiseArray = []
+
+        moduleNames.forEach((module) => {
+            const dir = path.resolve(`${module}/output`)
+            const promise = fse.rm(dir, { recursive: true })
+                .then(() => console.log("Deleted ", dir))
+                .catch(e => console.log("Caught", e));
+            promiseArray.push(promise);
+        });
+
+        Promise.allSettled(promiseArray).then(resolve).catch((e) => console.log(e));
+
+    })
+
+
+}
+
 
