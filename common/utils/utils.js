8a17fe7c37748cdcc7fc7ba23b0789761c3af4c4
Paras Anand <speed1992@gmail.com>
2021-12-20 09:55:52 +0530

    reverted minfy feature

--- common/utils/utils.js
+++ common/utils/utils.js
@@ -1,16 +1,16 @@
 var fse = require('fs-extra');
-const minify = require('node-json-minify')
 const { STATIC_SUBSTRING1, STATIC_SUBSTRING2 } = require("../constants/constants")
 
 const writeToFile = (json, { philosopherNameInSelector = null, varName }, MODULE_NAME, customContentToggle = false) => {
+
     const philosopher = `${varName.toLowerCase()}.json`
     const outputPath = `${MODULE_NAME}/output/${philosopher}`
 
     let content;
     if (customContentToggle)
-        content = `${STATIC_SUBSTRING1}${varName.toUpperCase()}${STATIC_SUBSTRING2}${minify(json)}`
+        content = `${STATIC_SUBSTRING1}${varName.toUpperCase()}${STATIC_SUBSTRING2}${JSON.stringify(json, null, 4)}`
     else
-        content = `${minify(json)}`
+        content = `${JSON.stringify(json, null, 4)}`
 
     return fse.outputFileSync(outputPath, content, err => {
         if (err) {
