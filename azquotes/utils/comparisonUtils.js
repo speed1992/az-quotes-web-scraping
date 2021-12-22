be4fb2265df7f8da51e5d06bb7b3c7af6370cbe5
Paras Anand <speed1992@gmail.com>
2021-12-22 08:05:39 +0530

    WIP

---
+++ azquotes/utils/comparisonUtils.js
@@ -0,0 +1,3 @@
+module.exports.checkAZquotesURLExists = function () {
+    return PHILOSOPHERS_DATA[j].azQuotesURL && PHILOSOPHERS_DATA[j].azQuotesURL != "" && typeof PHILOSOPHERS_DATA[j].azQuotesURL !== undefined
+}
