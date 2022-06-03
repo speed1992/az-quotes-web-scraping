b2da7f51339648eb1021912cbe3a1f4555a05d94
Paras Anand <qtparas@gmail.com>
2022-06-03 16:32:32 +0530

    filter code wip

--- filter-quotes-output/utils/utils.js
+++ filter-quotes-output/utils/utils.js
@@ -1,56 +1,17 @@
 
 const { FILTERS } = require("../constants/constants");
 
-function performCleanUp() {
+module.exports.splitFilters = function () {
     let newFilters = [];
 
     console.log("FILTERS before split", FILTERS)
 
     FILTERS.languageFilters.forEach((filterSentence, index) => {
-        const filtersAfterSplit = filterSentence.replaceAll(/\s/g, '').split('')
+        const filtersAfterSplit = filterSentence.replace(/\s/g, '').split('')
         newFilters = [...newFilters, ...filtersAfterSplit]
     });
 
-    console.log("FILTERS after split", newFilters)
+    return newFilters;
 }
 
-performCleanUp()
 
-/*
-
-function callback(err, data) {
-    if (err) console.log(err);
-    return data;
-}
-
-const parseOutput = (data) => JSON.parse(data);
-
-function readFileFromAllModules() {
-    return new Promise(function (resolve, _) {
-
-        for (let i = 0; i < PHILOSOPHERS_DATA.length; i++) {
-            const varName = PHILOSOPHERS_DATA[i].varName.toLowerCase();
-            let combinedOutput = [];
-
-            for (let j = 0; j < modules.length; j++) {
-                let output;
-                const inputPath = `../../${modules[j]}/output/${varName}.json`
-                try {
-                    output = fse.readFileSync(path.resolve(__dirname, inputPath), "utf8", callback)
-                    if (typeof output != undefined && output)
-                        combinedOutput = [...combinedOutput, ...(parseOutput(output))];
-                } catch (e) {
-                    console.log(e)
-                }
-
-            }
-            combinedOutput = [...new Set(combinedOutput)]
-            writeToFile(combinedOutput, { varName }, "combine-output")
-kk
-        }
-        resolve()
-    });
-
-}
-
-*/
