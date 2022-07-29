fe2f1e8a1e3f731a9b658d2ea6914f3465eb32f0
Paras Anand <speed1992@gmail.com>
2022-07-29 23:05:31 +0530

    constants file creation now part of main process

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
 const { start: pullQuotesFromAZQuotes } = require("./azquotes/app");
 const { start: combineOutputs } = require("./combine-output/app");
-const { start: removeDuplicateQuotes } = require("./string-similarity/app");
 const { start: pullQuotesFromGoodreads } = require("./goodreads/app");
 const { deleteOutputDirectories } = require("./delete-clear-outputs/app");
+const { start: makeConstantsFileForQuotesRepo } = require("./convert-constants-json/app");
 
 (async function () {
     await deleteOutputDirectories(["azquotes", "goodreads", "combine-output"])
     await pullQuotesFromAZQuotes();
     await pullQuotesFromGoodreads();
-    console.log("Combining process started.")
     combineOutputs();
+    makeConstantsFileForQuotesRepo();
 })();
-
 
 function errorHandler(error) {
     console.log(error);
--- convert-constants-json/app.js
+++ convert-constants-json/app.js
@@ -1,24 +1,6 @@
-// const { PHILOSOPHERS_DATA } = require('../common/constants/constants');
+const { PHILOSOPHERS_DATA } = require('../common/constants/constants');
 const { writeToFile } = require('../common/utils/utils');
-const { capitalize, hitTranslationAPI } = require('./utils/utils');
-
-const PHILOSOPHERS_DATA = [
-        {
-                azQuotesURL: "https://www.azquotes.com/author/13993-Joseph_Stalin",
-                goodreadsURL: "https://www.goodreads.com/author/quotes/138332.Joseph_Stalin",
-                varName: "STALIN"
-        },
-        {
-                azQuotesURL: "https://www.azquotes.com/author/11735-Plutarch",
-                goodreadsURL: "https://www.goodreads.com/author/quotes/31015.Plutarch",
-                varName: "PLUTARCH"
-        },
-        {
-                azQuotesURL: "https://www.azquotes.com/author/14903-Mike_Tyson",
-                goodreadsURL: "https://www.goodreads.com/author/quotes/6583167.Mike_Tyson",
-                varName: "TYSON"
-        }
-];
+const { capitalize } = require('./utils/utils');
 
 let obj = {
         value: "",
@@ -28,46 +10,36 @@ let obj = {
         }
 };
 
-module.exports.getConvertedObject = async function (philosopherInfo) {
+module.exports.getConvertedObject = function (philosopherInfo) {
         var newObj = { ...obj };
         newObj.value = philosopherInfo.varName;
         newObj.fullName = capitalize(philosopherInfo.varName);
 
-        // make a call to translate api
-        let translatedText = await hitTranslationAPI({ from: "en", to: "hi", inputText: newObj.fullName })
-        newObj.fullNameInOtherLanguages.hi = translatedText;
-
         console.log(newObj)
+
+        return newObj;
 
 };
 
 module.exports.convert = function () {
 
-        return new Promise(async (resolve, _) => {
+        var result = [];
 
-                var result = [];
+        for (var i = 0; i < PHILOSOPHERS_DATA.length; i++) {
+                let newObj = this.getConvertedObject(PHILOSOPHERS_DATA[i]);
 
-                for (var i = 0; i < PHILOSOPHERS_DATA.length; i++) {
-                        // console.log(philosopherInfo);
-                        let newObj = await this.getConvertedObject(PHILOSOPHERS_DATA[i]);
+                result.push(newObj);
 
-                        result.push(newObj);
-
-                        if (i === PHILOSOPHERS_DATA.length - 1) resolve(result);
-                }
-        });
-
+        }
+        return result;
 }
 
 module.exports.start = async function () {
 
-
-        let result = await this.convert();
+        let result = this.convert();
 
         console.log(result);
 
         writeToFile(result, { varName: "CONVERTED_CONSTANTS" }, "./");
 
 };
-
-this.start();
