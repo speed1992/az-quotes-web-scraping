6fa90c566a30c730a63da2fb14ee3381e5069b34
Paras Anand <qtparas@gmail.com>
2023-02-11 04:31:36 +0530

    Tested the whole flow, working fine

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ const { start: makeConstantsFileForQuotesRepo } = require("./convert-constants-j
 const { start: addIdsToTheCombinedOutput } = require("./add-ids-to-outputs/app");
 
 (async function () {
-    // await deleteOutputDirectories(["azquotes", "goodreads", "combine-output", "convert-constants-json"])
-    // await pullQuotesFromAZQuotes();
-    // await pullQuotesFromGoodreads();
-    // combineOutputs();
-    // await makeConstantsFileForQuotesRepo();
+    await deleteOutputDirectories(["azquotes", "goodreads", "combine-output", "convert-constants-json", "add-ids-to-outputs"])
+    await pullQuotesFromAZQuotes();
+    await pullQuotesFromGoodreads();
+    combineOutputs();
+    await makeConstantsFileForQuotesRepo();
     await addIdsToTheCombinedOutput();
-    
+
     console.log(`Two outputs generated:
     1. convert-constants-json/output/converted_constants.json
     2. add-ids-to-outputs/output`
